12457e59fd2800d4f428746f22730b1189b1394c
Daniel Antelo <daniel@daniels-mbp.mynet>
2017-04-02 20:48:46 +0100

    axios base url fix

--- README.md
+++ README.md
@@ -45,7 +45,7 @@ docker exec -it $(IMAGE_NAME) bash
 
 ### Locally (no docker)
 
-If you would like to get the project running without docker, you first have to edit app/webstore/config/xhr.js to use localhost instead of the linked container (uncomment line 2 and comment out line 3).
+If you would like to get the project running without docker, you first have to edit app/webstore/config/xhr.js to use localhost instead of the linked container (uncomment line 8).
 
 Then you should get the api up and running:
 
--- apps/webstore/config/xhr.js
+++ apps/webstore/config/xhr.js
@@ -1,6 +1,16 @@
+// @TODO make these urls configurable/injectable per environment
+// api:3000 is the docker linked container and will only work server side
+function getBaseUrl() {
+  if (typeof window !== 'undefined' && window.document) {
+    return 'http://localhost:3000/api';
+  }
+
+  // return 'http://localhost:3000/api'; // uncomment this if not using docker
+  return 'http://api:3000/api';
+}
+
 export default {
-  // baseURL: 'http://localhost:3000/api',
-  baseURL: 'http://api:3000/api',
+  baseURL: getBaseUrl(),
   timeout: 3000,
   headers: {
     'Content-Type': 'application/json',
